1b880649882c6dc2eb30031f409cbc312d4a6aec
Youssef Alm El Din <76973497+youssefalmeldin@users.noreply.github.com>
2025-01-16 16:10:09 +0200

    Add files via upload
    snapshout

---
+++ Library-Management-System-master/Library-Management-System-master/src/main/java/com/joo/librarymanagementsystem/repository/PublisherRepository.java
@@ -0,0 +1,9 @@
+package com.joo.librarymanagementsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.joo.librarymanagementsystem.entity.Publisher;
+
+public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+
+}
